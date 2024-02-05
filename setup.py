037aba2e908fca1b7b5cedec580fee79924ab0ec
davidting0918 <dting0918@gmail.com>
2024-02-05 10:30:33 +0800

    Update package files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cex_services",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",

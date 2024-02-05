1479e687560e756ae10e30d228ecff74647718d4
davidting0918 <dting0918@gmail.com>
2024-02-05 10:27:24 +0800

    Add operation files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cex_services",
+    version="1.0.2",
+    packages=find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)

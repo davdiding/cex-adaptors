affa193e5d1cabee7f348094f963332484bec65d
davidting0918 <dting0918@gmail.com>
2024-02-10 20:48:55 +0800

    Update OKX test codes

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-# from datetime import datetime as dt
+from datetime import datetime as dt
 from unittest import IsolatedAsyncioTestCase
 
 # from cex_services.binance import Binance
@@ -14,6 +14,61 @@ class TestOkx(IsolatedAsyncioTestCase):
         response = await okx.get_exchange_info()
         self.assertTrue(response)
 
+        await okx.close()
+        return
+
+    async def test_get_tickers(self):
+        okx = await Okx.create()
+        spot = await okx.get_tickers("spot")
+        self.assertTrue(spot)
+
+        perp = await okx.get_tickers("perp")
+        self.assertTrue(perp)
+
+        futures = await okx.get_tickers("futures")
+        self.assertTrue(futures)
+        await okx.close()
+        return
+
+    async def test_get_ticker(self):
+        okx = await Okx.create()
+        spot = await okx.get_ticker("BTC/USDT:USDT")
+        self.assertTrue(spot)
+
+        perp = await okx.get_ticker("BTC/USDT:USDT-PERP")
+        self.assertTrue(perp)
+
+        futures = await okx.get_ticker("BTC/USD:BTC-240329")
+        self.assertTrue(futures)
+        await okx.close()
+        return
+
+    async def test_get_klines(self):
+        okx = await Okx.create()
+        spot = await okx.get_klines("BTC/USDT:USDT", "1d", num=120)
+        self.assertEquals(len(spot), 120)
+
+        perp = await okx.get_klines("BTC/USDT:USDT-PERP", "1d", num=77)
+        self.assertEquals(len(perp), 77)
+
+        futures = await okx.get_klines("BTC/USD:BTC-240329", "1d", num=23)
+        self.assertEquals(len(futures), 23)
+        await okx.close()
+        return
+
+    async def test_get_klines_with_timestamp(self):
+        start = int(dt.timestamp(dt(2024, 1, 1)) * 1000)
+        end = int(dt.timestamp(dt(2024, 1, 31)) * 1000)
+
+        okx = await Okx.create()
+        spot = await okx.get_klines("BTC/USDT:USDT", "1d", start=start, end=end)
+        self.assertEquals(len(spot), 30)
+
+        perp = await okx.get_klines("BTC/USDT:USDT-PERP", "1d", start=start, end=end)
+        self.assertEquals(len(perp), 30)
+
+        futures = await okx.get_klines("BTC/USD:BTC-240329", "1d", start=start, end=end)
+        self.assertEquals(len(futures), 30)
         await okx.close()
         return
 

76e794735409cf17947b47e8efd6972fb2d4f016
davidting0918 <dting0918@gmail.com>
2024-02-22 10:35:13 +0800

    Add get_tickers in Bitget

--- cex_services/bitget.py
+++ cex_services/bitget.py
@@ -60,3 +60,35 @@ class Bitget(object):
                 linear_futures.update(sub_linear)
 
             return {**spot, **inverse, **linear_futures}
+
+    async def get_tickers(self, market_type: str = None):
+        if market_type:
+            if market_type == "spot":
+                return self.parser.parse_tickers(await self.bitget._get_spot_tickers(), self.exchange_info, "spot")
+            else:
+                derivative = {}
+                inverse = self.parser.parse_tickers(
+                    await self.bitget._get_derivative_tickers("COIN-FUTURES"), self.exchange_info, "derivative"
+                )
+                derivative.update(inverse)
+
+                for settle in self.parser.LINEAR_FUTURES_SETTLE:
+                    sub_linear = self.parser.parse_tickers(
+                        await self.bitget._get_derivative_tickers(f"{settle}-FUTURES"), self.exchange_info, "derivative"
+                    )
+                    derivative.update(sub_linear)
+
+                instrument_id = list(query_dict(self.exchange_info, f"is_{market_type} == True").keys())
+                return {k: v for k, v in derivative.items() if k in instrument_id}
+        else:
+            spot = self.parser.parse_tickers(await self.bitget._get_spot_tickers(), self.exchange_info, "spot")
+            inverse = self.parser.parse_tickers(
+                await self.bitget._get_derivative_tickers("COIN-FUTURES"), self.exchange_info, "derivative"
+            )
+            linear_futures = {}
+            for settle in self.parser.LINEAR_FUTURES_SETTLE:
+                sub_linear = self.parser.parse_tickers(
+                    await self.bitget._get_derivative_tickers(f"{settle}-FUTURES"), self.exchange_info, "derivative"
+                )
+                linear_futures.update(sub_linear)
+            return {**spot, **inverse, **linear_futures}
--- cex_services/exchanges/bitget.py
+++ cex_services/exchanges/bitget.py
@@ -14,3 +14,10 @@ class BitgetUnified(BaseClient):
     def _get_derivative_exchange_info(self, product_type: str):
         params = {"productType": product_type}
         return self._get(self.base_endpoint + "/api/v2/mix/market/contracts", params=params)
+
+    def _get_spot_tickers(self):
+        return self._get(self.base_endpoint + "/api/v2/spot/market/tickers")
+
+    def _get_derivative_tickers(self, product_type: str):
+        params = {"productType": product_type}
+        return self._get(self.base_endpoint + "/api/v2/mix/market/tickers", params=params)

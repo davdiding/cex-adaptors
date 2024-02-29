481a9456cdd3a3fe841e6941a94c191e6c7989e7
davidting0918 <dting0918@gmail.com>
2024-02-29 17:20:26 +0800

    Add okx private structure

--- cex_services/exchanges/okx.py
+++ cex_services/exchanges/okx.py
@@ -1,11 +1,55 @@
+import base64
+import hmac
+from datetime import datetime as dt
+
 from .base import BaseClient
 
 
 class OkxUnified(BaseClient):
     BASE_ENDPOINT = "https://www.okx.com"
 
-    def __init__(self):
+    def __init__(
+        self,
+        api_key: str,
+        api_secret: str,
+        passphrase: str,
+        use_server_time: bool = False,
+        debug: bool = False,
+        flag: str = "1",
+    ):
         super().__init__()
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.passphrase = passphrase
+        self.use_server_time = use_server_time
+        self.debug = debug
+        self.flag = flag
+
+    def get_private_header(self, method, request_path, body):
+        timestamp = self.get_timestamp()
+        message = str(timestamp) + str.upper(method) + request_path + body
+        sign = self.get_signature(message)
+
+        header = dict()
+        header["Content-Type"] = "application/json"
+        header["OK-ACCESS-KEY"] = self.api_key
+        header["OK-ACCESS-SIGN"] = sign
+        header["OK-ACCESS-TIMESTAMP"] = str(timestamp)
+        header["OK-ACCESS-PASSPHRASE"] = self.passphrase
+        header["x-simulated-trading"] = self.flag
+
+        return header
+
+    def get_signature(self, message: str):
+        mac = hmac.new(bytes(self.api_secret, encoding="utf8"), bytes(message, encoding="utf-8"), digestmod="sha256")
+        d = mac.digest()
+        return base64.b64encode(d).decode("utf-8")
+
+    @staticmethod
+    def get_timestamp():
+        now = dt.utcnow()
+        t = now.isoformat("T", "milliseconds")
+        return t + "Z"
 
     async def _get_exchange_info(self, instType: str) -> dict:
         return await self._get(self.BASE_ENDPOINT + "/api/v5/public/instruments", params={"instType": instType})
@@ -25,3 +69,13 @@ class OkxUnified(BaseClient):
         if limit:
             params["limit"] = limit
         return await self._get(self.BASE_ENDPOINT + "/api/v5/market/candles", params=params)
+
+    async def _get_balance(self, currency: str = None):
+        params = {}
+        if currency:
+            params["ccy"] = currency
+        request_path = "/api/v5/account/balance"
+
+        return await self._get(
+            self.BASE_ENDPOINT + request_path, params=params, headers=self.get_private_header("get", request_path, "")
+        )
